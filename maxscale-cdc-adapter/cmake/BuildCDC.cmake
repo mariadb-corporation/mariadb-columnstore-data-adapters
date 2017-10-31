1e1f949bf2de12669d72f65e7e9b0096e5779c59
Markus Mäkelä <markus.makela@mariadb.com>
2017-10-31 16:54:47 +0200

    Update CMake files
    The adapter now builds all required MariaDB libraries with the
    ExternalProject CMake module.

--- maxscale-cdc-adapter/cmake/BuildCDC.cmake
+++ maxscale-cdc-adapter/cmake/BuildCDC.cmake
@@ -1,16 +1,18 @@
 # Download and build the MaxScale CDC connector
 
-set(CDC_REPO "TODO: Add this" CACHE INTERNAL "MaxScale CDC connector")
+if (NOT DEFINED CDC_REPO)
+  message(FATAL_ERROR "Define path to the CDC connector repository with: -DCDC_REPO=/path/to/cdc_connector")
+endif()
 
-ExternalProject_Add(cdc-connector
- GIT_REPOSITORY ${CDC_REPO}
- CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/cdc-connector/install
- BINARY_DIR ${CMAKE_BINARY_DIR}/cdc-connector
- INSTALL_DIR ${CMAKE_BINARY_DIR}/cdc-connector/install
+ExternalProject_Add(cdc_connector
+ SOURCE_DIR ${CDC_REPO}
+ CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/cdc_connector/install
+ BINARY_DIR ${CMAKE_BINARY_DIR}/cdc_connector
+ INSTALL_DIR ${CMAKE_BINARY_DIR}/cdc_connector/install
  UPDATE_COMMAND "")
 
 set(CDC_FOUND TRUE CACHE INTERNAL "")
 set(CDC_STATIC_FOUND TRUE CACHE INTERNAL "")
-set(CDC_INCLUDE_DIR ${CMAKE_BINARY_DIR}/cdc-connector/install/include CACHE INTERNAL "")
-set(CDC_STATIC_LIBRARIES ${CMAKE_BINARY_DIR}/cdc-connector/install/lib/libcdc-connector.a CACHE INTERNAL "")
+set(CDC_INCLUDE_DIR ${CMAKE_BINARY_DIR}/cdc_connector/install/include CACHE INTERNAL "")
+set(CDC_STATIC_LIBRARIES ${CMAKE_BINARY_DIR}/cdc_connector/install/${CMAKE_INSTALL_LIBDIR}/libcdc_connector.a CACHE INTERNAL "")
 set(CDC_LIBRARIES ${CDC_STATIC_LIBRARIES} CACHE INTERNAL "")

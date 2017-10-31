a1b0a359de0fab6a7d5020840e1b8e4715ba34c1
Markus Mäkelä <markus.makela@mariadb.com>
2017-10-31 22:04:13 +0200

    Build CDC connector using the GitHub repository
    The CDC connector now has a GitHub repository so the CMake files can be
    updated to use that.

--- maxscale-cdc-adapter/cmake/BuildCDC.cmake
+++ maxscale-cdc-adapter/cmake/BuildCDC.cmake
@@ -1,18 +1,14 @@
 # Download and build the MaxScale CDC connector
 
-if (NOT DEFINED CDC_REPO)
-  message(FATAL_ERROR "Define path to the CDC connector repository with: -DCDC_REPO=/path/to/cdc_connector")
-endif()
-
-ExternalProject_Add(cdc_connector
- SOURCE_DIR ${CDC_REPO}
- CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/cdc_connector/install
- BINARY_DIR ${CMAKE_BINARY_DIR}/cdc_connector
- INSTALL_DIR ${CMAKE_BINARY_DIR}/cdc_connector/install
+ExternalProject_Add(cdc-connector
+ GIT_REPOSITORY "https://github.com/mariadb-corporation/maxscale-cdc-connector"
+ CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/cdc-connector/install
+ BINARY_DIR ${CMAKE_BINARY_DIR}/cdc-connector
+ INSTALL_DIR ${CMAKE_BINARY_DIR}/cdc-connector/install
  UPDATE_COMMAND "")
 
 set(CDC_FOUND TRUE CACHE INTERNAL "")
 set(CDC_STATIC_FOUND TRUE CACHE INTERNAL "")
-set(CDC_INCLUDE_DIR ${CMAKE_BINARY_DIR}/cdc_connector/install/include CACHE INTERNAL "")
-set(CDC_STATIC_LIBRARIES ${CMAKE_BINARY_DIR}/cdc_connector/install/${CMAKE_INSTALL_LIBDIR}/libcdc_connector.a CACHE INTERNAL "")
+set(CDC_INCLUDE_DIR ${CMAKE_BINARY_DIR}/cdc-connector/install/include CACHE INTERNAL "")
+set(CDC_STATIC_LIBRARIES ${CMAKE_BINARY_DIR}/cdc-connector/install/${CMAKE_INSTALL_LIBDIR}/libcdc_connector.a CACHE INTERNAL "")
 set(CDC_LIBRARIES ${CDC_STATIC_LIBRARIES} CACHE INTERNAL "")

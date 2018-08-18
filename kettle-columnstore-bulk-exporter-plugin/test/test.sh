93b7d1c925f2b3d6602691138c841004254f4485
sneJ- <jens.rowekamp@mariadb.com>
2018-08-17 18:45:19 -0700

    MCOL-1644 - Added PDI 8.1 to Linux kettle test suite

--- kettle-columnstore-bulk-exporter-plugin/test/test.sh
+++ kettle-columnstore-bulk-exporter-plugin/test/test.sh
@@ -27,81 +27,94 @@ if [ $# -ge 1 ]; then
 	fi
 fi
 
-pdi7zipFile="pdi-ce-7.1.0.0-12.zip"
+# These arrays define the different PDI versions to use for testing, please ensure that the positions of the entries match.
+pdiVersionArray=("7.1" "8.1")
+pdiDownloadLinkPathArray=("https://svwh.dl.sourceforge.net/project/pentaho/Data%20Integration/7.1/" "https://ayera.dl.sourceforge.net/project/pentaho/Pentaho%208.1/client-tools/")
+pdiDownloadFileNameArray=("pdi-ce-7.1.0.0-12.zip" "pdi-ce-8.1.0.0-365.zip")
 
+# environment setup
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" #get the absolute diretory of this script
 
-# download and install PDI 7
-if [ ! -d $DIR/data-integration ] ; then
-	echo "PDI installation not found"
-	if [ ! -f $DIR/$pdi7zipFile ] ; then
-		echo "PDI 7 zip file not found - initiate download"
+# setup the PDI test environments
+for i in `seq 0 $((${#pdiVersionArray[*]}-1))`
+do
+	# download and install the defined PDI versions
+	if [ ! -d $DIR/${pdiVersionArray[i]}/data-integration ] ; then
+		echo "PDI ${pdiVersionArray[i]} installation not found"
+		if [ ! -f $DIR/${pdiDownloadFileNameArray[i]} ] ; then
+			echo "PDI ${pdiVersionArray[i]} zip file not found - initiate download"
+		else
+			echo "old PDI ${pdiVersionArray[i]} zip file found - delete it and restart download"
+			rm $DIR/${pdiDownloadFileNameArray[i]}
+		fi
+		curl -o $DIR/${pdiDownloadFileNameArray[i]} ${pdiDownloadLinkPathArray[i]}${pdiDownloadFileNameArray[i]}
+		echo "installing PDI ${pdiVersionArray[i]} from ${pdiDownloadFileNameArray[i]}"
+		unzip -q $DIR/${pdiDownloadFileNameArray[i]} -d $DIR/${pdiVersionArray[i]}
+		rm $DIR/${pdiDownloadFileNameArray[i]}
 	else
-		echo "old PDI 7 zip file found - delete it and restart download"
-		rm $DIR/$pdi7zipFile
+		echo "PDI ${pdiVersionArray[i]} installation found"
 	fi
-	curl -o $DIR/$pdi7zipFile https://svwh.dl.sourceforge.net/project/pentaho/Data%20Integration/7.1/$pdi7zipFile
-	echo "installing PDI 7 from $pdi7zipFile"
-	unzip -q $DIR/$pdi7zipFile -d $DIR
-	rm $DIR/$pdi7zipFile
-else
-	echo "PDI installation found"
-fi
 
-# add the jdbc mariadb client library
-if [ ! -f $DIR/data-integration/lib/mariadb-java-client-2.2.3.jar ]; then
-	echo "download and install mariadb's java client into PDI"
-	curl -o $DIR/data-integration/lib/mariadb-java-client-2.2.3.jar https://downloads.mariadb.com/Connectors/java/connector-java-2.2.3/mariadb-java-client-2.2.3.jar
-else
-	echo "mariadb's java client found in PDI"
-fi
+	# add the jdbc mariadb client library
+	if [ ! -f $DIR/${pdiVersionArray[i]}/data-integration/lib/mariadb-java-client-2.2.3.jar ]; then
+		echo "download and install mariadb's java client into PDI ${pdiVersionArray[i]}"
+		curl -o $DIR/${pdiVersionArray[i]}/data-integration/lib/mariadb-java-client-2.2.3.jar https://downloads.mariadb.com/Connectors/java/connector-java-2.2.3/mariadb-java-client-2.2.3.jar
+	else
+		echo "mariadb's java client found in PDI ${pdiVersionArray[i]}"
+	fi
 
-# install the columnstore plugin
-if [ -d $DIR/data-integration/plugins/mariadb-columnstore-kettle-bulk-exporter-plugin ]; then
-	echo "deleting old pdi columnstore bulk exporter plugin"
-	rm -rf $DIR/data-integration/plugins/mariadb-columnstore-kettle-bulk-exporter-plugin
-fi
-echo "installing fresh columnstore bulk exporter plugin"
-unzip -q $DIR/../build/distributions/mariadb-columnstore-kettle-bulk-exporter-plugin-*.zip -d $DIR/data-integration/plugins
+	# install the columnstore plugin
+	if [ -d $DIR/${pdiVersionArray[i]}/data-integration/plugins/mariadb-columnstore-kettle-bulk-exporter-plugin ]; then
+		echo "deleting old PDI ${pdiVersionArray[i]} columnstore bulk exporter plugin"
+		rm -rf $DIR/${pdiVersionArray[i]}/data-integration/plugins/mariadb-columnstore-kettle-bulk-exporter-plugin
+	fi
+	echo "installing fresh columnstore bulk exporter plugin in PDI ${pdiVersionArray[i]}"
+	unzip -q $DIR/../build/distributions/mariadb-columnstore-kettle-bulk-exporter-plugin-*.zip -d $DIR/${pdiVersionArray[i]}/data-integration/plugins
+done
 
 # delete old test logs
-numberOfOldLogFiles=`find $DIR/tests -name *.kjb.log | wc -l`
+numberOfOldLogFiles=`find $DIR/tests -name *.kjb.*.log | wc -l`
 if [ $numberOfOldLogFiles -gt 0 ]; then
 	echo "deleting old test log files"
-	for f in $( find $DIR/tests -name *.kjb.log ); do
+	for f in $( find $DIR/tests -name *.kjb.*.log ); do
 		rm $f
 	done
 fi
 
 # executing tests
-echo ""
-echo "test execution:"
-failed=0
 set +e                          #stop exiting as soon as any line in the bash script fails
-for t in $( find $DIR/tests -name *.kjb ); do
-	echo "test $t"
-	# load variable from job.parameter
-	jobparameter=""
-	if [ -f `dirname $t`/job.parameter ]; then
-		parfile=`dirname $t`/job.parameter
-		IFS=$'\r\n' GLOBIGNORE='*' command eval 'properties=($(cat $parfile))'
-		for prop in "${properties[@]}"; do
-			jobparameter=$jobparameter" -param:$prop"
-		done
-	fi
-	# execute test
-	$DIR/data-integration/kitchen.sh -file=$t -level=Detailed $jobparameter > $t.log
-	if [ $? -eq 0 ]; then
-		echo "test $t passed"
-	else
-		echo "test $t failed"
-		failed=1
-	fi
+failed=0
+
+for i in `seq 0 $((${#pdiVersionArray[*]}-1))`
+do
+	echo ""
+	echo "test execution with PDI ${pdiVersionArray[i]}:"
+	for t in $( find $DIR/tests -name *.kjb ); do
+		echo "test $t"
+		# load variable from job.parameter
+		jobparameter=""
+		if [ -f `dirname $t`/job.parameter ]; then
+			parfile=`dirname $t`/job.parameter
+			IFS=$'\r\n' GLOBIGNORE='*' command eval 'properties=($(cat $parfile))'
+			for prop in "${properties[@]}"; do
+				jobparameter=$jobparameter" -param:$prop"
+			done
+		fi
+		# execute test
+		$DIR/${pdiVersionArray[i]}/data-integration/kitchen.sh -file=$t -level=Detailed $jobparameter > $t.${pdiVersionArray[i]}.log
+		if [ $? -eq 0 ]; then
+			echo "test $t passed"
+		else
+			echo "test $t failed"
+			failed=$(($failed+1))
+		fi
+	done
 done
+
 if [ $failed -eq 0 ]; then
 	echo "all tests passed"
-	exit 0
 else
-	echo "some tests failed"
-	exit 1
+	echo "$failed tests failed"
 fi
+
+exit $failed
